5de5ec539a90e47d6ba88e2cc052c89da988812f
Matthew Mazankowski <m.g.mazankowski@gmail.com>
2022-04-20 17:40:57 -0600

    form and list working

--- QuoteApp/Controllers/HomeController.cs
+++ QuoteApp/Controllers/HomeController.cs
@@ -18,17 +18,31 @@ namespace QuoteApp.Controllers
         //    _logger = logger; 
         //}
 
+        private QuoteDbContext _quoteContext { get; set; }
+
         private iQuoteRepository _repo { get; set; }
 
-        public HomeController(iQuoteRepository temp)
+        public HomeController(iQuoteRepository temp, QuoteDbContext someQuote)
         {
-            _repo = temp; 
+            _repo = temp;
+            _quoteContext = someQuote; 
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var quotes = _quoteContext.Quotes
+                .OrderBy(x => x.Author)
+                .ToList();
+
+            return View(quotes);
         }
+
+        //[HttpPost]
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
         [HttpGet]
         public IActionResult QuoteForm()
@@ -39,7 +53,45 @@ namespace QuoteApp.Controllers
         [HttpPost]
         public IActionResult QuoteForm(Quote q)
         {
-            return View("Confirmation");
+            _quoteContext.Add(q);
+            _quoteContext.SaveChanges();
+            return View("Confirmation", q);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int quoteid)
+        {
+            ViewBag.Quotes = _quoteContext.Quotes.ToList();
+
+            var quote = _quoteContext.Quotes.Single(x => x.QuoteId == quoteid);
+
+            return View("QuoteForm", quote);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Quote q)
+        {
+            _quoteContext.Update(q);
+            _quoteContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int quoteid)
+        {
+            var quote = _quoteContext.Quotes.Single(x => x.QuoteId == quoteid);
+
+            return View(quote);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Quote q)
+        {
+            _quoteContext.Quotes.Remove(q);
+            _quoteContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
--- QuoteApp/Models/QuoteDbContext.cs
+++ QuoteApp/Models/QuoteDbContext.cs
@@ -13,8 +13,65 @@ namespace QuoteApp.Models
 
         public DbSet<Quote> Quotes { get; set; }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //}
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Quote>().HasData(
+
+                new Quote
+                {
+                    QuoteId = 1,
+                    TheQuote = "I've had enough trouble for a lifetime",
+                    Author = "Harry Potter",
+                    Date = "07/30/1997",
+                    Subject = "Contentment",
+                    Citation = "983jf9skdhjf93"
+                },
+                new Quote
+                {
+                    QuoteId = 2,
+                    TheQuote = "Whether you think you can or you can't, you're right!",
+                    Author = "Henry Ford",
+                    Date = "05/01/1904",
+                    Subject = "Inspirational",
+                    Citation = "3c9j390192r3ss3"
+                },
+                new Quote
+                {
+                    QuoteId = 3,
+                    TheQuote = "Fortune favors the brave",
+                    Author = "Virgil",
+                    Date = "01/01/1500",
+                    Subject = "Inspirational",
+                    Citation = "192f9djrk939283j3"
+                },
+                new Quote
+                {
+                    QuoteId = 4,
+                    TheQuote = "Out of the mountain of despair, a stone of hope",
+                    Author = "Martin Luther King Jr.",
+                    Date = "06/18/1972",
+                    Subject = "Faith",
+                    Citation = "234k39fj3k3"
+                },
+                new Quote
+                {
+                    QuoteId = 5,
+                    TheQuote = "It is our choices that show what we truly are, far more than our abilities",
+                    Author = "Albus Dumbledore",
+                    Date = "07/07/1994",
+                    Subject = "Inspirational",
+                    Citation = "a3jsdf034uikf932"
+                },
+                new Quote
+                {
+                    QuoteId = 6,
+                    TheQuote = "If you can dream it, you can do it",
+                    Author = "Walt Disney",
+                    Date = "02/12/1950",
+                    Subject = "Motivational",
+                    Citation = "s0993jf9j2k22"
+                }
+            );
+        }
     }
 }
